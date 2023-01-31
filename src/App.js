3c57e681d04be89a73ea05e9aa30f502e11b980b
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2023-01-31 15:35:55 +0500

    app.js
    added bootstrap and navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import React, { Component } from 'react'
+import Navbar from "./components/Navbar";
 
 export default class App extends Component {
   a = "Sanwal"
   render() {
     return (
-      <div className="App">App {this.a}</div>
+      <div>
+      <Navbar/>
+      </div>
     )
   }
 }
