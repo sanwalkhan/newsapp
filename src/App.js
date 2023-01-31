ac449db761fef4d530bbf056623217ce732ae9b7
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2023-01-31 20:03:39 +0500

    newsitem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import React, { Component } from 'react'
 import Navbar from "./components/Navbar";
+import News from "./components/news";
 
 export default class App extends Component {
-  a = "Sanwal"
+  // a = "Sanwal"
   render() {
     return (
       <div>
       <Navbar/>
+      <News/>
       </div>
     )
   }
